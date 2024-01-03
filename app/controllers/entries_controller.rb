c43e750e456fefd8fb06263e1d83de48c2a7444f
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-01-03 13:55:45 +0100

    feat: redirect to the homepage after creating a new entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -25,7 +25,7 @@ class EntriesController < ApplicationController
 
     respond_to do |format|
       if @entry.save
-        format.html { redirect_to entry_url(@entry), notice: 'Entry was successfully created.' }
+        format.html { redirect_to entries_url, notice: 'Entry was successfully created.' }
         format.json { render :show, status: :created, location: @entry }
       else
         format.html { render :new, status: :unprocessable_entity }
